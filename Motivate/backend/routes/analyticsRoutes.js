2a94cd0b80cf8fa277bf8f189a7114bf4b597aef
Süleyman Altan <suleymanaltan.277@gmail.com>
2025-06-30 17:55:14 +0300

    Update analyticsRoutes.js

--- Motivate/backend/routes/analyticsRoutes.js
+++ Motivate/backend/routes/analyticsRoutes.js
@@ -4,6 +4,5 @@ const auth = require("../middleware/authMiddleware");
 const { getWeeklyAnalytics } = require("../controllers/analyticsController");
 
 router.get("/weekly", auth, getWeeklyAnalytics); 
-// /api/analytics/weekly?startDate=2025-04-14&endDate=2025-04-20
 
 module.exports = router;
